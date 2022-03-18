3d8171601e0df3409ec40642fa323b00ad36cc41
Bravo6GD <messiashraya@gmail.com>
2022-03-18 10:20:37 +0545

    Through php

--- Hiweeb.php
+++ Hiweeb.php
@@ -1,11 +1,6 @@
 <?php
 
-$to_email = "kusal.bista@sifal.deerwalk.edu.np";
-$subject = "Simple Email Test via PHP";
-$body = "Namaskar Nepali le payo vane garxa";
-$headers = "From: KP Oli";
-if (mail($to_email, $subject, $body, $headers)) {
-    echo "Email successfully sent to $to_email...";
-} else {
-    echo "Email sending failed...";
-}
+echo"Hello World";
+echo"Nameste World";
+echo"etseman World";
+?>
